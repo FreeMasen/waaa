8c78861b35448b8a06962f2e42c969b40c6c666b
Robert Masen <rmasen@apexrevtech.com>
2019-08-21 15:48:16 -0500

    closer still

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,37 @@ use std::io::Write;
 use crossterm_cursor::{cursor, TerminalCursor};
 type Res<T> = Result<T, Box<dyn std::error::Error>>;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct FinalProduct {
+    id: i64,
+    name: String,
+    manufacturer: String,
+    macros: Macro,
+    serving: ServingSize
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Macro {
+    calories: f64,
+    carbs: f64,
+    fat: f64,
+    protein: f64,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ServingSize {
+    raw: SingleServing,
+    household: SingleServing, 
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct SingleServing {
+    value: f64,
+    units: String,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -222,45 +253,33 @@ fn main() -> Res<()> {
     let en_ct = conn.execute("INSERT INTO energy
                 SELECT products.id, nutrients.value
                 FROM products
-                LEFT OUTER JOIN nutrients
+                LEFT JOIN nutrients
                 ON products.id = nutrients.food_id
-                and (
-                    nutrients.id = ?1
-                    or nutrients.id is NULL
-                )", &[&208])?;
+                and nutrients.id = ?1", &[&208])?;
     println!("energy count: {}\ncreating carbs table", en_ct);
     conn.execute("CREATE TABLE carbs (food_id INTEGER, value REAL DEFAULT 0)", NO_PARAMS)?;
     let c_ct = conn.execute("INSERT INTO carbs
                 SELECT products.id, nutrients.value
                 FROM products
-                LEFT OUTER JOIN nutrients
+                LEFT JOIN nutrients
                 ON products.id = nutrients.food_id
-                and (
-                    nutrients.id = ?1
-                    or nutrients.id is NULL
-                )", &[&205])?;
+                and nutrients.id = ?1", &[&205])?;
     println!("carbs count:{}\ncreating fat table", c_ct);
     conn.execute("CREATE TABLE fat (food_id INTEGER, value REAL DEFAULT 0)", NO_PARAMS)?;
     let f_ct = conn.execute("INSERT INTO fat
                 SELECT products.id, nutrients.value
                 FROM products
-                LEFT OUTER JOIN nutrients
+                LEFT JOIN nutrients
                 ON products.id = nutrients.food_id
-                and (
-                    nutrients.id = ?1
-                    or nutrients.id is NULL
-                )", &[&204])?;
+                and nutrients.id = ?1", &[&204])?;
     println!("fat count: {}\ncreating protein table", f_ct);
     conn.execute("CREATE TABLE protein (food_id INTEGER, value REAL DEFAULT 0)", NO_PARAMS)?;
     let p_ct = conn.execute("INSERT INTO protein
                 SELECT products.id, nutrients.value
                 FROM products
-                LEFT OUTER JOIN nutrients
+                LEFT JOIN nutrients
                 ON products.id = nutrients.food_id
-                and (
-                    nutrients.id = ?1
-                    or nutrients.id is NULL
-                )", &[&203])?;
+                and nutrients.id = ?1", &[&203])?;
     println!("protein count: {}\ninserting values into temp_nut", p_ct);
     
     println!("food id count: {}", id_ct);
@@ -277,9 +296,78 @@ fn main() -> Res<()> {
                 join protein
                     on food_ids.id = protein.food_id",
     NO_PARAMS)?;
+    conn.execute("CREATE TABLE final_products (id INTEGER, name TEXT, manufacturer TEXT,
+                value REAL, unit STRING, household_value REAL, household_unit TEXT,
+                calories REAL, carbs REAL, fat REAL, protein REAL);", NO_PARAMS)?;
+    conn.execute("INSERT INTO final_products
+                SELECT products.id, products.name, products.manufacturer,
+                serving.value, serving.unit, serving.household_value, serving.household_unit,
+                temp_nut.energy, temp_nut.carbs, temp_nut.fat, temp_nut.protein
+        FROM products
+            JOIN serving
+                ON products.id = serving.food_id
+            JOIN temp_nut
+                ON products.id = temp_nut.food_id;", NO_PARAMS)?;
+    let mut fin = conn.prepare(
+        "SELECT products.id, products.name, products.manufacturer,
+                serving.value, serving.unit, serving.household_value, serving.household_unit,
+                temp_nut.energy, temp_nut.carbs, temp_nut.fat, temp_nut.protein
+        FROM products
+            JOIN serving
+                ON products.id = serving.food_id
+                and (serving.value is not null 
+                    or serving.household_value is not null)
+            JOIN temp_nut
+                ON products.id = temp_nut.food_id
+                and temp_nut.energy is not null
+                and temp_nut.carbs is not null
+                and temp_nut.fat is not null
+                and temp_nut.protein is not null
+                and temp_nut.energy > 0
+                and temp_nut.carbs > 0
+                and temp_nut.fat > 0
+                and temp_nut.protein > 0
+        "
+    )?;
     println!("full count: {}\n", full_ct);
     assert_eq!(full_ct, id_ct);
-    conn.execute_batch("DROP TABLE energy;DROP TABLE carbs;DROP TABLE fat;DROP TABLE protein;DROP TABLE food_ids;")?;
+    let result: Vec<FinalProduct> = fin.query_map(NO_PARAMS, |row|{ 
+    let id: i64 = row.get(0).expect("failed to get 1 as id");
+    let name: String = row.get(1).expect("failed to get 2 as name");
+    let manufacturer: String = row.get(2).expect("failed to get 3 as manufacturer");
+    let value: Option<f64> = row.get(3).expect("failed to get 4 as value");
+    let units: String = row.get(4).expect("failed to get 5 as units");
+    let household_value: Option<f64> = row.get(5).expect("failed to get 6 as household_value");
+    let household_units: String = row.get(6).expect("failed to get 7 as household_units");
+    let calories: f64 = row.get(7)?;
+    let carbs: f64 = row.get(8)?;
+    let fat: f64 = row.get(9)?;
+    let protein: f64 = row.get(10)?;
+    Ok(
+        FinalProduct {
+            id,
+            name,
+            manufacturer,
+            serving: ServingSize {
+                raw: SingleServing {
+                    units,
+                    value: value.unwrap_or(0f64),
+                },
+                household: SingleServing {
+                    value: household_value.unwrap_or(0f64),
+                    units: household_units,
+                },
+            },
+            macros: Macro {
+                calories: calories,
+                carbs: carbs,
+                fat: fat,
+                protein: protein,
+            }
+        }
+    )})?.filter_map(|p| p.ok()).collect();
+    ::std::fs::write("test.json", serde_json::to_string_pretty(&result)?)?;
+    conn.execute_batch("DROP TABLE energy;DROP TABLE carbs;DROP TABLE fat;DROP TABLE protein;DROP TABLE food_ids;DROP TABLE temp_nut;")?;
     println!("Starting backup\n");
     let mut on_disk = rusqlite::Connection::open("nutr.sqlite")?;
     rusqlite::backup::Backup::new(&conn, &mut on_disk)?.run_to_completion(
@@ -337,4 +425,41 @@ where T: serde::de::DeserializeOwned + serde::ser::Serialize + GetId + Debug {
     }
     // b.write(b"\n]")?;
     Ok(())
+}
+
+fn from_row(row: rusqlite::Row) -> Res<FinalProduct> {
+    let id: i64 = row.get(1)?;
+    let name: String = row.get(2)?;
+    let manufacturer: String = row.get(3)?;
+    let value: f64 = row.get(4)?;
+    let units: String = row.get(5)?;
+    let household_value: f64 = row.get(6)?;
+    let household_units: String = row.get(7)?;
+    let calories: f64 = row.get(8)?;
+    let carbs: f64 = row.get(9)?;
+    let fat: f64 = row.get(10)?;
+    let protein: f64 = row.get(11)?;
+    Ok(
+        FinalProduct {
+            id,
+            name,
+            manufacturer,
+            serving: ServingSize {
+                raw: SingleServing {
+                    units,
+                    value,
+                },
+                household: SingleServing {
+                    value: household_value,
+                    units: household_units,
+                },
+            },
+            macros: Macro {
+                calories,
+                carbs,
+                fat,
+                protein
+            }
+        }
+    )
 }
